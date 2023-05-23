91135f8d0cf2e5d31f1f77b8e67dc0d1ab25237e
EgrShishov <112828095+EgrShishov@users.noreply.github.com>
2023-05-23 21:54:32 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func work(n int) int {
+	if n > 3 {
+		time.Sleep(time.Millisecond * 500)
+		return n + 1
+	} else {
+		time.Sleep(time.Millisecond * 500)
+		return n - 1
+	}
+}
+
+func main() {
+	_map := make(map[int]int)
+	for i := 0; i < 10; i++ {
+		var c = 0
+		fmt.Scan(&c)
+		if value, inMap := _map[c]; inMap {
+			fmt.Println(value)
+		} else {
+			_map[c] = work(c)
+			fmt.Println(c)
+		}
+	}
+
+}
