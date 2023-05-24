0d19d28bee67ecda9ecc2840e77b7cf749e7de88
EgrShishov <e.shishov98@yandex.ru>
2023-05-24 15:45:49 +0300

    add new task

--- main.go
+++ main.go
@@ -1,37 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"unicode"
-)
+import "fmt"
 
-func adding() int64 {
-	var s1, s2 string
-	var clear1, clear2 string
-	fmt.Scan(&s1, &s2)
-
-	for _, char := range s1 {
-		if unicode.IsDigit(char) {
-			clear1 += string(char)
-		}
-	}
-	ans1, _err1 := strconv.ParseInt(clear1, 10, 64)
-
-	for _, char := range s2 {
-		if unicode.IsDigit(char) {
-			clear2 += string(char)
-		}
-	}
-
-	ans2, _err2 := strconv.ParseInt(clear2, 10, 64)
-
-	if _err1 == nil && _err2 == nil {
-		return ans2 + ans1
-	}
-	return 0
-}
-
+// Дается строка. Нужно удалить все символы, которые встречаются более одного раза и вывести получившуюся строку
 func main() {
-	fmt.Print(adding())
+	var s string
+	fmt.Scan(&s)
+
+	_map := make(map[rune]int)
+
+	for _, char := range s {
+		_map[char]++
+	}
+	var ans string
+	for char, value := range _map {
+		if value == 1 {
+			ans += string(char)
+		}
+	}
+
+	fmt.Print(ans)
 }
