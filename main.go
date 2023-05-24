74dd1d0c8f581a095b926a6a281692be111c2001
EgrShishov <e.shishov98@yandex.ru>
2023-05-24 15:18:00 +0300

    add dima

--- main.go
+++ main.go
@@ -2,30 +2,36 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strconv"
+	"unicode"
 )
 
-func work(n int) int {
-	if n > 3 {
-		time.Sleep(time.Millisecond * 500)
-		return n + 1
-	} else {
-		time.Sleep(time.Millisecond * 500)
-		return n - 1
-	}
-}
+func adding() int64 {
+	var s1, s2 string
+	var clear1, clear2 string
+	fmt.Scan(&s1, &s2)
 
-func main() {
-	_map := make(map[int]int)
-	for i := 0; i < 10; i++ {
-		var c = 0
-		fmt.Scan(&c)
-		if value, inMap := _map[c]; inMap {
-			fmt.Println(value)
-		} else { //i love dima
-			_map[c] = work(c)
-			fmt.Println(c)
+	for _, char := range s1 {
+		if unicode.IsDigit(char) {
+			clear1 += string(char)
+		}
+	}
+	ans1, _err1 := strconv.ParseInt(clear1, 10, 64)
+
+	for _, char := range s2 {
+		if unicode.IsDigit(char) {
+			clear2 += string(char)
 		}
 	}
 
+	ans2, _err2 := strconv.ParseInt(clear2, 10, 64)
+
+	if _err1 == nil && _err2 == nil {
+		return ans2 + ans1
+	}
+	return 0
+}
+
+func main() {
+	fmt.Print(adding())
 }
