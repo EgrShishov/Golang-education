ea81639aa1c2988e742e2b19dfcb5c7ba9bcc5e1
EgrShishov <e.shishov98@yandex.ru>
2023-06-04 13:47:11 +0300

    api json task

--- main.go
+++ main.go
@@ -1,23 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
 
-// Дается строка. Нужно удалить все символы, которые встречаются более одного раза и вывести получившуюся строку
+type Specialities struct {
+	Name                                string `json:"name"`
+	FacultyId                           int    `json:"facultyId"`
+	FacultyName                         string `json:"facultyName"`
+	SpecialityDepartmentEducationFormId int    `json:"specialityDepartmentEducationFormId"`
+	SpecialityName                      string `json:"specialityName"`
+	Course                              int    `json:"course"`
+	Id                                  int    `json:"id"`
+	CalendarId                          string `json:"calendarId"`
+}
+
+type Faculties struct {
+	Name   string `json:"name"`
+	Abbrev string `json:"abbrev"`
+	Id     int    `json:"id"`
+}
+
+func FacultiesParse(client *http.Client) {
+	response, err := client.Get("https://iis.bsuir.by/api/v1/faculties")
+	if err != nil {
+		fmt.Printf("There are some error with response body : %s", err)
+		return
+	}
+
+	//defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Cant read response body %s", err)
+		return
+	}
+
+	var facultiesData []Faculties
+	err = json.Unmarshal(body, &facultiesData)
+	if err != nil {
+		fmt.Printf("Cant parse JSON : %s", err)
+		return
+	}
+
+	fmt.Println("----------------------FacultiesInfo--------------------------------------")
+	for _, elem := range facultiesData {
+		fmt.Println(elem.Abbrev, " ", elem.Id, " ", elem.Name)
+	}
+}
+
+func StudentGroupsParse(client *http.Client) {
+	response, err := client.Get("https://iis.bsuir.by/api/v1/student-groups")
+	if err != nil {
+		fmt.Printf("There are some errors with requst : %s", err)
+		return
+	}
+	//defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Cant read response body : %s", err)
+		return
+	}
+
+	var data []Specialities
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Printf("Cant parse JSON: %s", err)
+		return
+	}
+
+	fmt.Println("----------------------Students Groups---------------------------")
+	for _, elem := range data {
+		fmt.Println(elem.Id, " ", elem.CalendarId, " ", elem.Course, " ", elem.Name, " ", elem.FacultyName, " ", elem.FacultyId, " ", elem.SpecialityName, " ", elem.SpecialityDepartmentEducationFormId)
+	}
+}
+
 func main() {
-	var s string
-	fmt.Scan(&s)
 
-	_map := make(map[rune]int)
+	client := http.Client{}
 
-	for _, char := range s {
-		_map[char]++
-	}
-	var ans string
-	for char, value := range _map {
-		if value == 1 {
-			ans += string(char)
-		}
-	}
-
-	fmt.Print(ans)
+	StudentGroupsParse(&client)
+	FacultiesParse(&client)
 }
